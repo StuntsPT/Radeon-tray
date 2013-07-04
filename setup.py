a367e35f4cdbbf2a27a13986f98e36ac73ad08d8
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-07-04 22:57:15 +0100

    Corrected a few bugs with setup.py. Mostly path related

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 from setuptools import setup
+import glob
 
 setup(name='Radeon-tray', packages=['radeon-tray'],
 description='A small program to control the power profiles of your Radeon card via systray icon.',
 url='https://github.com/StuntsPT/Radeon-tray', install_requires=['PyQt>=4.0'],
-data_files=[('assets', ['assets/*']), ('/lib/systemd/system', ['systemd/radeonpm.service']),
-('/usr/bin', ['bin/Radeon-tray.py']), ('/usr/sbin', ['bin/radeonpmserver.py'])])
+data_files=[('share/pixmaps', glob.glob('radeon-tray/assets/*')), ('lib/systemd/system', ['radeon-tray/systemd/radeonpm.service']),
+('bin', ['radeon-tray/bin/Radeon-tray.py']), ('sbin', ['radeon-tray/bin/radeonpmserver.py'])])
