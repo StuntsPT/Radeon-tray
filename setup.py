6b9a7f742c8b692bff015cd11610c0e484e9c30b
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-07-04 23:37:07 +0100

    Another stab at correcting interperter problems.

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@
 from setuptools import setup
 import glob
 
+scripts = ['Radeon-tray.py', 'radeonpmserver.py']
+
+
 setup(name='Radeon-tray', packages=['radeon_tray'],
 description='A small program to control the power profiles of your Radeon card via systray icon.',
 url='https://github.com/StuntsPT/Radeon-tray', install_requires=['PyQt>=4.0'],
+scripts=['bin/' + x for x in scripts]
 data_files=[('share/pixmaps', glob.glob('radeon_tray/assets/*')), ('lib/systemd/system', ['radeon_tray/systemd/radeonpm.service']),
 ('bin', ['radeon_tray/bin/Radeon-tray.py']), ('sbin', ['radeon_tray/bin/radeonpmserver.py'])])
